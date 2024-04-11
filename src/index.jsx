a224a16d3b1cea785135add7870ecb94da473092
Giovani Drosda Lima <gdrosdalima@gmail.com>
2024-04-11 11:08:29 -0300

    gh-pages fix

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from './Theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
   </React.StrictMode>,
 )
